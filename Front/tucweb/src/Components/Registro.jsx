ab8c0d83759e3469b9658fc7268381a39f222ee6
CinRivadeneira <cistri092@gmail.com>
2023-02-07 18:03:41 -0300

    registro

--- Front/tucweb/src/Components/Registro.jsx
+++ Front/tucweb/src/Components/Registro.jsx
@@ -1,27 +1,73 @@
+import React from "react";
 import "../Css/Registro.css";
 
 const Registro = () => {
+
+
+
     return (
-        <div className ="contenedor">
+        <div className="contenedor">
             <div className="container">
                 <div className="login-container">
                     <div className="register">
                         <h2>Registrarse</h2>
-                        <form action="">
-                            <input type="text" placeholder="Nombre" className="nombre" />
-                            <input type="text" placeholder="Correo" className="correo" />
-                            <input type="password" placeholder="Contraseña" className="pass" />
-                            <input type="submit" className="submit" value="REGISTRARSE" />
+                        <form>
+                            <div className="mb-3">
+                                <b>Nombre</b>
+                                <input
+                                    type="text"
+                                    className="nombre"
+                                    placeholder="Ingrese su nombre"
+                                    name="firstname"
+
+                                    required
+                                />
+                            </div>
+                            <div className="mb-3">
+                                <b>Correo electrónico</b>
+                                <input
+                                    type="email"
+                                    className="correo"
+                                    placeholder="example@example.com"
+                                    name="email"
+
+
+                                    required
+                                />
+                            </div>
+
+                            <div className="mb-3">
+                                <b>Contraseña</b>
+                                <input
+                                    type="password"
+                                    className="pass"
+                                    placeholder="Ingrese su contraseña"
+                                    name="password"
+
+
+                                    required
+                                />
+                            </div>
+                            <button className="boton"> Registrarse</button>
                         </form>
                     </div>
+
                     <div className="login">
                         <h2> Iniciar sesión</h2>
-                        <div className="login-items">
-                            <input type="text" placeholder="Usuario" className="usuario" />
-                            <input type="password" placeholder="Contraseña" className="contraseña" />
-                            <button className="boton">INGRESAR
-                            </button>
-                        </div>
+                        <input
+                            type="text"
+                            placeholder="Usuario"
+                            className="usuario"
+                            required
+                        />
+                        <input
+                            type="password"
+                            placeholder="Contraseña"
+                            className="contraseña"
+                            required
+                        />
+                        <br />
+                        <button className="boton"> Ingresar </button>
                     </div>
                 </div>
             </div>
